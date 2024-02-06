e78e442a294e519750d07d9150e4ce705286f607
Lemon_Juiced <tbrotheawesome@gmail.com>
2024-02-06 16:55:09 -0500

    Better folder architecture

--- scripts/headerFooterManager.js
+++ scripts/headerFooterManager.js
@@ -4,25 +4,25 @@ class CustomNavBar extends HTMLElement {
         `
         <ul class="navbar">
             <li class="dropdown">
-                <a href="index.html" class="dropbtn">Home</a>
+                <a href="https://lemonjuice.dev/index.html" class="dropbtn">Home</a>
             </li>
             <li class="dropdown">
-                <a href="esoteric_programming_languages.html" class="dropbtn">Esoteric Programming Languages</a>
+                <a href="https://lemonjuice.dev//html/esolangs/esoteric_programming_languages.html" class="dropbtn">Esoteric Programming Languages</a>
                 <div class="dropdown-content">
-                    <a href="cscroll_jscroll.html">CScroll/JScroll</a>
-                    <a href="cscroll_jscroll_documentation.html">CScroll/JScroll Documentation</a>
+                    <a href="https://lemonjuice.dev//html/esolangs/cscroll_jscroll.html">CScroll/JScroll</a>
+                    <a href="https://lemonjuice.dev//html/esolangs/cscroll_jscroll_documentation.html">CScroll/JScroll Documentation</a>
                 </div>
             </li>
             <li class="dropdown">
-                <a href="minecraft_mods.html">Minecraft Mods</a>
+                <a href="https://lemonjuice.dev//html/mods/minecraft_mods.html">Minecraft Mods</a>
                 <div class="dropdown-content">
-                    <a href="attributions.html">Attributions</a>
-                    <a href="contest_entry.html">Contest Entry</a>
-                    <a href="contributions.html">Contributions</a>
-                    <a href="dependencies.html">Dependencies</a>
-                    <a href="developement_progress.html">Development Progress</a>
-                    <a href="discontinued_mods.html">Discontinued Mods</a>
-                    <a href="old_ideas.html">Old Ideas</a>
+                    <a href="https://lemonjuice.dev//html/mods/attributions.html">Attributions</a>
+                    <a href="https://lemonjuice.dev//html/mods/contest_entry.html">Contest Entry</a>
+                    <a href="https://lemonjuice.dev//html/mods/contributions.html">Contributions</a>
+                    <a href="https://lemonjuice.dev//html/mods/dependencies.html">Dependencies</a>
+                    <a href="https://lemonjuice.dev//html/mods/developement_progress.html">Development Progress</a>
+                    <a href="https://lemonjuice.dev//html/mods/discontinued_mods.html">Discontinued Mods</a>
+                    <a href="https://lemonjuice.dev//html/mods/old_ideas.html">Old Ideas</a>
                 </div>
             </li>   
         </ul>

2425bb390f6efde6d631e5faba7bb429e170fb09
Lemon_Juiced <tbrotheawesome@gmail.com>
2024-02-06 17:22:00 -0500

    Reverted hard links

--- scripts/headerFooterManager.js
+++ scripts/headerFooterManager.js
@@ -4,25 +4,57 @@ class CustomNavBar extends HTMLElement {
         `
         <ul class="navbar">
             <li class="dropdown">
-                <a href="https://lemonjuice.dev/index.html" class="dropbtn">Home</a>
+                <a href="../index.html" class="dropbtn">Home</a>
             </li>
             <li class="dropdown">
-                <a href="https://lemonjuice.dev/html/esolangs/esoteric_programming_languages.html" class="dropbtn">Esoteric Programming Languages</a>
+                <a href="../esolangs/esoteric_programming_languages.html" class="dropbtn">Esoteric Programming Languages</a>
                 <div class="dropdown-content">
-                    <a href="https:/lemonjuice.dev/html/esolangs/cscroll_jscroll.html">CScroll/JScroll</a>
-                    <a href="https://lemonjuice.dev/html/esolangs/cscroll_jscroll_documentation.html">CScroll/JScroll Documentation</a>
+                    <a href="../esolangs/cscroll_jscroll.html">CScroll/JScroll</a>
+                    <a href="../esolangs/cscroll_jscroll_documentation.html">CScroll/JScroll Documentation</a>
                 </div>
             </li>
             <li class="dropdown">
-                <a href="https://lemonjuice.dev/html/mods/minecraft_mods.html">Minecraft Mods</a>
+                <a href="../mods/minecraft_mods.html">Minecraft Mods</a>
                 <div class="dropdown-content">
-                    <a href="https://lemonjuice.dev/html/mods/attributions.html">Attributions</a>
-                    <a href="https://lemonjuice.dev/html/mods/contest_entry.html">Contest Entry</a>
-                    <a href="https://lemonjuice.dev/html/mods/contributions.html">Contributions</a>
-                    <a href="https://lemonjuice.dev/html/mods/dependencies.html">Dependencies</a>
-                    <a href="https://lemonjuice.dev/html/mods/developement_progress.html">Development Progress</a>
-                    <a href="https://lemonjuice.dev/html/mods/discontinued_mods.html">Discontinued Mods</a>
-                    <a href="https://lemonjuice.dev/html/mods/old_ideas.html">Old Ideas</a>
+                    <a href="../mods/attributions.html">Attributions</a>
+                    <a href="../mods/contest_entry.html">Contest Entry</a>
+                    <a href="../mods/contributions.html">Contributions</a>
+                    <a href="../mods/dependencies.html">Dependencies</a>
+                    <a href="../mods/developement_progress.html">Development Progress</a>
+                    <a href="../mods/discontinued_mods.html">Discontinued Mods</a>
+                    <a href="../mods/old_ideas.html">Old Ideas</a>
+                </div>
+            </li>   
+        </ul>
+        `
+    }
+}
+
+class CustomHomepageNavBar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 
+        `
+        <ul class="navbar">
+            <li class="dropdown">
+                <a href="index.html" class="dropbtn">Home</a>
+            </li>
+            <li class="dropdown">
+                <a href="esolangs/esoteric_programming_languages.html" class="dropbtn">Esoteric Programming Languages</a>
+                <div class="dropdown-content">
+                    <a href="esolangs/cscroll_jscroll.html">CScroll/JScroll</a>
+                    <a href="esolangs/cscroll_jscroll_documentation.html">CScroll/JScroll Documentation</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="mods/minecraft_mods.html">Minecraft Mods</a>
+                <div class="dropdown-content">
+                    <a href="mods/attributions.html">Attributions</a>
+                    <a href="mods/contest_entry.html">Contest Entry</a>
+                    <a href="mods/contributions.html">Contributions</a>
+                    <a href="mods/dependencies.html">Dependencies</a>
+                    <a href="mods/developement_progress.html">Development Progress</a>
+                    <a href="mods/discontinued_mods.html">Discontinued Mods</a>
+                    <a href="mods/old_ideas.html">Old Ideas</a>
                 </div>
             </li>   
         </ul>
@@ -31,3 +63,4 @@ class CustomNavBar extends HTMLElement {
 }
 
 customElements.define('custom-navbar', CustomNavBar)
+customElements.define('custom-homepage-navbar', CustomHomepageNavBar)
